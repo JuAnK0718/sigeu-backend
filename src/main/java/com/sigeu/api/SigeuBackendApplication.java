a2aa4253baf4f97005159417435b1c4a5b1b29cb
Camilo <eljuankomg@gmail.com>
2026-04-26 22:22:25 -0500

    Initial commit: Spring Boot 21 project setup

---
+++ src/main/java/com/sigeu/api/SigeuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sigeu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigeuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigeuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sigeu/api/SigeuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigeu.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
